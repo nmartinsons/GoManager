ff77c5bfb0d57de5c3ff2fe5c4c158fd32d1ab0e
Niks Martinsons <66245053+nmartinsons@users.noreply.github.com>
2023-11-30 11:33:46 +0200

    Added functions and db connection

--- app/connection.go
+++ app/connection.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var db *sql.DB
+
+func connect() {
+	var err error
+
+	// Open the SQLite database file
+	db, err = sql.Open("sqlite3", "GoData.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check if the connection is successful
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to the database")
+}
+
+func closeConnection() {
+	if db != nil {
+		db.Close()
+		fmt.Println("Connection closed")
+	}
+}
--- app/main.go
+++ app/main.go
@@ -1,1 +1,43 @@
+// 1. Create tasks
+// 2. View tasks
+// 3. Upadate tasks
+// 4. Delete tasks
+// 5. Displaying number of completed tasks
+
 package main
+
+func main() {
+	// Call the connect function to establish a connection
+	connect()
+	defer closeConnection()
+
+	// CRUD application logic goes here
+
+	
+	// insertData()
+	// fetchData()
+	// updateData()
+	// deleteData()
+	//displayCompleteTasks
+}
+
+func createData() {
+	
+}
+
+func viewData() {
+	
+}
+
+func updateData() {
+	
+}
+
+func deleteData() {
+	
+}
+
+func displayCompleteTasks() {
+	
+}
+
