24386e8ff8a8843b3b49e6805a75878e575a8abf
Niks Martinsons <66245053+nmartinsons@users.noreply.github.com>
2023-12-03 20:59:00 +0200

    Added Update and Delete functionality

--- app/main.go
+++ app/main.go
@@ -13,6 +13,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -111,13 +112,90 @@ func viewTasks() {
 
 
 func updateTask() {
-	
+	fmt.Print("Enter the title of the task to update: ")
+	var oldTitle string
+	fmt.Scanln(&oldTitle)
+
+	oldTitle = strings.TrimSpace(oldTitle)
+
+    var existingTask Task
+    err := db.QueryRow("SELECT title, description, dueDate, priority, status FROM tasks WHERE title LIKE ?", "%"+oldTitle+"%").Scan(&existingTask.title, &existingTask.description, &existingTask.dueDate, &existingTask.priority, &existingTask.status)
+    if err != nil {
+        if err == sql.ErrNoRows {
+            fmt.Println("Task not found")
+        } else {
+            log.Fatal(err)
+        }
+        return
+    }
+
+
+	fmt.Println("\nExisting Task Details:")
+	fmt.Printf("Title: %sDescription: %sDue Date: %s\nPriority: %d\nStatus: %s\n", existingTask.title, existingTask.description, existingTask.dueDate, existingTask.priority, existingTask.status)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter the new title of the task: ")
+	newTitle, _ := reader.ReadString('\n')
+	newTitle = strings.TrimSpace(newTitle)
+
+	fmt.Print("Enter the new description of the task: ")
+	newDescription, _ := reader.ReadString('\n')
+	newDescription = strings.TrimSpace(newDescription)
+
+	fmt.Print("Enter the new due date of the task (format: DD-MM-YYYY): ")
+	newDueDateStr, _ := reader.ReadString('\n')
+	newDueDate, err := time.Parse("02-01-2006", newDueDateStr[:len(newDueDateStr)-1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Enter the new priority of the task: ")
+	newPriorityStr, _ := reader.ReadString('\n')
+	newPriority, err := strconv.Atoi(newPriorityStr[:len(newPriorityStr)-1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Enter the new status of the task: ")
+	newStatus, _ := reader.ReadString('\n')
+	newStatus = strings.TrimSpace(newStatus)
+
+	_, err = db.Exec("UPDATE tasks SET title = ?, description = ?, dueDate = ?, priority = ?, status = ? WHERE title = ?", newTitle, newDescription, newDueDate, newPriority, newStatus, oldTitle)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("\nTask updated successfully!")
 }
 
 
 
 func deleteTask() {
+	fmt.Print("Enter the title of the task to delete: ")
+	var titleToDelete string
+	fmt.Scanln(&titleToDelete)
+
+	titleToDelete = strings.TrimSpace(titleToDelete)
+
+	var existingTaskTitle Task
+	err := db.QueryRow("SELECT title, description, dueDate, priority, status FROM tasks WHERE title LIKE ?", "%"+titleToDelete+"%").Scan(&existingTaskTitle.title, &existingTaskTitle.description, &existingTaskTitle.dueDate, &existingTaskTitle.priority, &existingTaskTitle.status)
+    if err != nil {
+        if err == sql.ErrNoRows {
+            fmt.Println("Task not found")
+        } else {
+            log.Fatal(err)
+        }
+        return
 	
+	}
+
+	_, err = db.Exec("DELETE FROM tasks WHERE title LIKE ?", titleToDelete)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("\nTask deleted successfully!")
 }
 
 
