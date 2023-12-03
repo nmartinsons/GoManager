851aa4eb2dcfc67699b0579120b1d72ada5d2968
Niks Martinsons <66245053+nmartinsons@users.noreply.github.com>
2023-12-03 14:20:33 +0200

    added main function functionality, other updates

--- app/main.go
+++ app/main.go
@@ -10,6 +10,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -31,6 +32,7 @@ func connect() {
 		log.Fatal(err)
 	}
 
+	// print success message if connected
 	fmt.Println("Connected to the database")
 }
 
@@ -53,6 +55,10 @@ func createTask() {
 	
 }
 
+func viewTasks() {
+	
+}
+
 func updateTask() {
 }
 
@@ -69,9 +75,40 @@ func displayCompleteTasks() {
 
 
 func main() {
-	// Call the connect function to establish a connection
+	// connect function is being called to establish a connection
 	connect()
 
 	defer closeConnection()
+
+	for {
+		fmt.Println("\nOptions:")
+		fmt.Println("1. Create task")
+		fmt.Println("2. View tasks")
+		fmt.Println("3. Update task")
+		fmt.Println("4. Delete task")
+		fmt.Println("5. Display completed tasks")
+		fmt.Println("6. Exit")
+
+		var choice int
+		fmt.Print("Enter your choice: ")
+		fmt.Scanln(&choice)
+
+		switch choice {
+		case 1:
+			createTask()
+		case 2:
+			viewTasks()
+		case 3:
+			updateTask()
+		case 4:
+			deleteTask()
+		case 5:
+			displayCompleteTasks()
+		case 6:
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid choice. Please try again.")
+		}
+	}
 
 }
